a6fa1232df10ea4740854accf3ae32c2aa1c1870
Dmitry Zimoykin <zimoykin@icloud.com>
2020-11-26 00:50:00 +0300

    ${10}

--- src/Pages/Lists/PostsList.js
+++ src/Pages/Lists/PostsList.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import * as K from '../Models/Constants'
 
-function PostsList(props) {
+function PostsList( props ) {
 
     return (
 
@@ -11,64 +11,77 @@ function PostsList(props) {
                 <table>
                     {props.posts.map((post, index) => {
                         return <tr> <td>
-                                    <div>
-                                        <img className='mt-0 pt-1 d-block w-100' src={`${post.image}`} />
-                                    </div>
-                                    <div className='text-center bg-dark w-100'>
-                                        <a className='text-light bg-dark text-uppercasse font-weight-light' href={"/posts/" + post.id}> {(post.title)} </a>
-                                    </div>
-                                    <div style={{backgroundColor: '#f0ece4'}}>
-                                    <p> {(post.description)} </p>
-                                    </div>
+                            <a href={"/posts/" + post.id}>
 
-                                </td></tr>
+                                <div>
+                                    <img className='mt-0 pt-1 d-block w-100' src={`${post.image}`} />
+                                </div>
+                                <div className='text-center bg-dark w-100'>
+                                    <span className='text-light bg-dark text-uppercasse font-weight-light'> {(post.title)} </span>
+                                </div>
+
+                            </a>
+                            <div style={{ backgroundColor: '#f0ece4' }}>
+                                <p> {(post.description)} </p>
+                            </div>
+
+                        </td></tr>
                     })}
                 </table>
             </div>
             :
-            <div className="container h-100">
-                <table>{props.posts.map((post, index) => {
-                    return <tr><td>
+            <div className="container bg-light">
 
-                        <div><table>
-                            <tr>
-                                <td>
-                                    <div className='h-100'>
-                                        <img className='mt-0 pt-0 d-block w-100 pr-1' src={`${post.image}`} />
+                {props.posts.map((post, index) => {
+
+                    return (
+                        <div>  <div>
+                            <div className="row">
+                                {/* col */}
+                                <div className="col-5">
+                                    <div className='h-100 pr-3' sttyle={{ maxHeight: '200px' }}>
+                                        <a href={"/posts/" + post.id}>  <div style={{ height: '250px', width: '450px', borderRadius: '10px', backgroundPosition: 'center', backgroundImage: `url(${post.image})` }} />  </a>
                                     </div>
-                                </td>
+                                </div>
 
-                                <td>
-                                    <div className='h-100'>
-                                        <div className="postHeader text-uppercase">
-                                            <a href={"/posts/" + post.id}>
-                                                {(post.title)}
-                                            </a>
-                                        </div>
-                                        <div className="postDescription font-weight-light"> <p>
-                                            {(post.description)}</p>
-                                        </div>
+                                {/* col */}
+                                <div className="col-7">
+
+                                    <div className="postHeader text-uppercase w-100">
+                                        <a href={"/posts/" + post.id}>
+                                            {(post.title)}
+                                        </a>
                                     </div>
-                                </td>
 
+                                    <div className="postDescription font-weight-light w-100"> <p>
+                                        {(post.description)}</p>
+                                    </div>
 
-                            </tr>
+                                </div>
 
-                            <tr><td>
-                                <div style={{ valign: 'bottom' }} className="d-flex bg-light pb-3">{post.tags.map((tag) => {
-                                    return <div> <a href={"/search/tag/" + tag}> {"#" + tag} </a>  <span>&nbsp;</span> </div>
+                            </div>
+
+                            <div className='row'>
+                                <div style={{ valign: 'bottom' }} className="d-flex bg-light pb-3 pl-3 w-100">{post.tags.map((tag) => {
+                                    return <div> <a style={{ color:'darkgray', textAlign: 'justify'}} href={"/search/tag/" + tag}> {"#" + tag} </a>  <span>&nbsp;</span>
+                                    </div>
                                 })}
                                 </div>
-                            </td></tr>
-                        </table>
-                        </div>
-                    </td>
-                    </tr>
-                })}
-                </table>
-            </div>
-    )
 
-}
+                            </div>
+                        </div> </div>
+
+
+                    )
+
+                })}
+            </div>
+
+     )
+ }
+
+                       
+                                
+                            
 
 export default PostsList
